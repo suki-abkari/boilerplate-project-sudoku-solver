0ae93d04ec33cc708a11c61d17f4b1a89742c769
Suki <devacount.ab220@gmail.com>
2021-11-30 12:53:27 +0100

    POST /api/solve & validate function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,10 +1,14 @@
 class SudokuSolver {
-
+  // The validate function should take a given puzzle string and check it to see if it has 81 valid characters for the input.
   validate(puzzleString) {
+      // check if invalid characters
+    if(puzzleString.match(/[^1-9\.]/g)) {
+       return {status : 'valid'}
+    }   
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+ 
   }
 
   checkColPlacement(puzzleString, row, column, value) {
@@ -16,8 +20,12 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+    // validate puzzle
+    let validate = this.validate(puzzleString)
+    if (validate.error) { return validate }
+
   }
+
 }
 
 module.exports = SudokuSolver;
--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,29 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
 
     });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
 
-    });
+  // You can POST /api/solve with form data containing puzzle which will be a string containing a combination of numbers (1-9) and periods . to represent empty spaces. The returned object will contain a solution property with the solved puzzle.
+  app.route("/api/solve").post((req, res) => {
+    const puzzleString = req.body.puzzle;
+
+    // If the object submitted to /api/solve is missing puzzle
+    if (!puzzleString) return res.json({ error: "Required field missing" });
+
+    // If the puzzle submitted to /api/solve is greater or less than 81 characters
+    if (!solver.validate(puzzleString))
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+
+     const solution = solver.solve(puzzleString);
+    // If the puzzle submitted to /api/solve is invalid or cannot be solved
+    if (!solution) return res.json({ error: 'Puzzle cannot be solved' });
+    res.json({ solution });
+  });
 };
