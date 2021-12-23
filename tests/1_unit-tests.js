0fe0a333b3ec69e40f146d2b45b49d2462b3616f
Suki Abkari <69718320+suki-abkari@users.noreply.github.com>
2021-12-23 11:19:08 +0100

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,16 +5,27 @@ const Solver = require('../controllers/sudoku-solver.js');
 const puzzlesAndSolutions = require('../controllers/puzzle-strings');
 let solver = new Solver();
 
+let IinvalidPuzzle = 'X.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'
+
+
 suite('UnitTests', () => {
   suite('Function validate(puzzleString)', function() {
-
+    // valid puzzle string of 81 characters
     test('logic handles a valid puzzle string of 81 characters', function(done) {
-
+    
       let input = puzzlesAndSolutions.puzzlesAndSolutions[0]
       input = input[0].split(',')
       input = input[0];
       assert.equal(solver.validate(input), true);
       done();
+    });
+    // puzzle string with invalid characters (not 1-9 or .)
+    test('logic handles a puzzle string with invalid characters (not 1-9 or .)', function(done) {
+      
+        let result = "Invalid characters in puzzle"
+        assert.equal(solver.solve(IinvalidPuzzle).error, result);
+        done();
+      
     });
   })
 });
